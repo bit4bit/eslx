cec83da9cd297dc6eb2a3d2a6fcf74715e399881
Jovany Leandro G.C <bit4bit@riseup.net>
2023-03-23 14:05:02 -0500

    fix: license

--- test/support/stub_tcp_server/internal.ex
+++ test/support/stub_tcp_server/internal.ex
@@ -1,6 +1,3 @@
-# Copyright 2022 Picallex Holding Group. All rights reserved.
-#
-# @author (2022) Jovany Leandro G.C <jovany@picallex.com>
 defmodule StubTCPServer.Internal do
   def handle_opened(server, conn) do
     send(server, {:opened, conn})
