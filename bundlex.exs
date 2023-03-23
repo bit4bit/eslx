72ea3dda8b841323a28661749834394349916b0a
Jovany Leandro G.C <bit4bit@riseup.net>
2023-03-22 21:56:59 -0500

    feat: first compilation of freeswitch esl.c

--- bundlex.exs
+++ bundlex.exs
@@ -2,6 +2,22 @@ defmodule Toxico.BundlexProject do
   use Bundlex.Project
 
   def project() do
-    []
+    [
+      libs: libs()
+    ]
+  end
+
+  defp libs() do
+    freeswitch_esl_sources =
+      File.ls!("c_src/esl/freeswitch_esl")
+      |> Enum.filter(&(String.ends_with?(&1, [".c", ".h"])))
+
+    [
+      freeswitch_esl: [
+        sources: freeswitch_esl_sources,
+        includes: ["c_src/esl/freeswitch_esl/include"],
+        src_base: "esl/freeswitch_esl"
+      ]
+    ]
   end
 end
