1aa6753fe6c8e8fcde83e3752c3042b34d396efb
Jovany Leandro G.C <bit4bit@riseup.net>
2023-03-22 21:34:14 -0500

    initial

---
+++ lib/eslx.ex
@@ -0,0 +1,18 @@
+defmodule Eslx do
+  @moduledoc """
+  Documentation for `Eslx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Eslx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/eslx_test.exs
@@ -0,0 +1,8 @@
+defmodule EslxTest do
+  use ExUnit.Case
+  doctest Eslx
+
+  test "greets the world" do
+    assert Eslx.hello() == :world
+  end
+end
